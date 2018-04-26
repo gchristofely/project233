7cadbc6969bb0f096cba096dda7b8166f8da6a74
gchristofely <30760670+gchristofely@users.noreply.github.com>
2018-04-26 10:10:28 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+// app.js
+
+window.addEventListener('load', function() {
+
+  var webAuth = new auth0.WebAuth({
+    domain: 'project233.auth0.com',
+    clientID: 'V-l6SsxA-Dncl55ZvYzFNRMQBRdW-Rhy',
+    responseType: 'token id_token',
+    audience: 'https://project233.auth0.com/userinfo',
+    scope: 'openid',
+    redirectUri: window.location.href
+  });
+
+  var loginBtn = document.getElementById('btn-login');
+
+  loginBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    webAuth.authorize();
+  });
+
+});
